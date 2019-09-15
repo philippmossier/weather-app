10d0c96fb75716164d2d6616fbd6fb0aa4b25f47
Philipp Mossier <p.mossier.development@gmail.com>
2019-09-15 22:55:31 +0200

    refactored app, geocode and forecast file

--- utils.js/forecast.js
+++ utils.js/forecast.js
@@ -3,13 +3,13 @@ const request = require("request")
 const forecast = (latitude, longitude, callback) => {
     const url = `https://api.darksky.net/forecast/e5ee0a6d5bf1fbf6ef820e8613d408e6/${latitude},${longitude}?lang=de&units=si`
 
-    request({ url: url, json: true }, (error, response) => {
+    request({ url: url, json: true }, (error, { body }) => {
         if (error) {
             callback('unable to connect to weather service!', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('unable to find location', undefined)
         } else {
-            callback(undefined, `It's currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability} % chance of rain.`)
+            callback(undefined, `It's currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability} % chance of rain.`)
         }
     })
 
